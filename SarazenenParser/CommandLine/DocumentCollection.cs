1866a4ce4f5be0a604d7a8f6b4271ba3258ef841
nsaef <saefnasrin@gmail.com>
2016-12-19 10:44:08 +0100

    teiHeader class structure

--- SarazenenParser/CommandLine/DocumentCollection.cs
+++ SarazenenParser/CommandLine/DocumentCollection.cs
@@ -8,12 +8,19 @@ using System.Xml.Serialization;
 
 namespace CommandLine {
 
-    [XmlRootAttribute("root", Namespace = "http://www.tei-c.org/ns/1.0", IsNullable = false)]
+    [XmlRootAttribute("teiCorpus", Namespace = "http://www.tei-c.org/ns/1.0", IsNullable = false)]
     public class DocumentCollection {
         public DocumentCollection() {
             Documents = new List<ParsedDocument>();
+            SourceDesc sourceDesc = new SourceDesc("Beschreibender Text");
+            FileDesc fileDesc = new FileDesc("Sarazenen", "Literatursammlung zu Sarazenen der Bonner Mediaevisten", sourceDesc);
+            header = new TeiHeader(fileDesc);
         }
 
+        [XmlElement(ElementName = "teiHeader")]
+        public TeiHeader header { get; set; }
+
+        [XmlElement(ElementName = "teiCorpus", Namespace = "http://www.tei-c.org/ns/1.0")]
         public List<ParsedDocument> Documents { get; set; }
     }
 }
